b3870a640f85c0f846e0b5b74bf17222b6e1f626
iii <cclan8@outlook.com>
2019-04-29 20:04:51 +0800

    合并摄像头与视频捕捉，并与img调换文件名

--- run.py
+++ run.py
@@ -1,16 +1,14 @@
 import argparse
 import logging
-import sys
 import time
 
-from tf_pose import common
 import cv2
 import numpy as np
+
 from tf_pose.estimator import TfPoseEstimator
 from tf_pose.networks import get_graph_path, model_wh
 
-logger = logging.getLogger('TfPoseEstimatorRun')
-logger.handlers.clear()
+logger = logging.getLogger('TfPoseEstimator-WebCam')
 logger.setLevel(logging.DEBUG)
 ch = logging.StreamHandler()
 ch.setLevel(logging.DEBUG)
@@ -18,75 +16,58 @@ formatter = logging.Formatter('[%(asctime)s] [%(name)s] [%(levelname)s] %(messag
 ch.setFormatter(formatter)
 logger.addHandler(ch)
 
+fps_time = 0
+
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='tf-pose-estimation run')
-    parser.add_argument('--image', type=str, default='./images/p1.jpg')
-    parser.add_argument('--model', type=str, default='cmu',
-                        help='cmu / mobilenet_thin / mobilenet_v2_large / mobilenet_v2_small')
+    # 参数分析
+    parser = argparse.ArgumentParser(description='tf-pose-estimation realtime video')
+    parser.add_argument('--video', default=0, help="the source of video, default is camera")
     parser.add_argument('--resize', type=str, default='0x0',
-                        help='if provided, resize images before they are processed. '
-                             'default=0x0, Recommends : 432x368 or 656x368 or 1312x736 ')
+                        help='if provided, resize images before they are processed. default=0x0, Recommends : 432x368 or 656x368 or 1312x736 ')
     parser.add_argument('--resize-out-ratio', type=float, default=4.0,
-                        help='if provided, resize heatmaps before they are post-processed. default=1.0')
-
+                        help='if provided, resize heatmaps before they are post-processed. default=4.0')
+    parser.add_argument('--model', type=str, default='cmu', help='cmu / mobilenet_thin / mobilenet_v2_large / mobilenet_v2_small')
+    parser.add_argument('--show-process', type=bool, default=False,
+                        help='for debug purpose, if enabled, speed for inference is dropped.')
+    parser.add_argument('--showBG', type=bool, default=True, help='False to show skeleton only.')
     args = parser.parse_args()
 
+    # 模型初始化
+    logger.debug('initialization %s : %s' % (args.model, get_graph_path(args.model)))
     w, h = model_wh(args.resize)
-    if w == 0 or h == 0:
-        e = TfPoseEstimator(get_graph_path(args.model), target_size=(432, 368))
-    else:
-        e = TfPoseEstimator(get_graph_path(args.model), target_size=(w, h))
+    e = TfPoseEstimator(get_graph_path(args.model), target_size=(w, h))
+    logger.debug('video read+')
 
-    # estimate human poses from a single image !
-    image = common.read_imgfile(args.image, None, None)
-    if image is None:
-        logger.error('Image can not be read, path=%s' % args.image)
-        sys.exit(-1)
+    # 捕捉视频
+    video = cv2.VideoCapture(args.video)
+    ret_val, image = video.read()
 
-    t = time.time()
-    humans = e.inference(image, resize_to_default=(w > 0 and h > 0), upsample_size=args.resize_out_ratio)
-    elapsed = time.time() - t
+    while video.isOpened:
+        ret_val, image = video.read()
+        # 视频读取结束
+        if not ret_val:
+            break
 
-    logger.info('inference image: %s in %.4f seconds.' % (args.image, elapsed))
+        humans = e.inference(image, resize_to_default=(w > 0 and h > 0), upsample_size=args.resize_out_ratio)
+            
+        logger.debug(humans)
 
-    image = TfPoseEstimator.draw_humans(image, humans, imgcopy=False)
+        if not args.showBG:
+            image = np.zeros(image.shape)
+        image = TfPoseEstimator.draw_humans(image, humans, imgcopy=False)
 
-    try:
-        import matplotlib.pyplot as plt
+        # 输出FPS
+        cv2.putText(image,
+                    "FPS: %f" % (1.0 / (time.time() - fps_time)),
+                    (10, 10),  cv2.FONT_HERSHEY_SIMPLEX, 0.5,
+                    (0, 255, 0), 2)
+        cv2.imshow('Human motion', image)
+        fps_time = time.time()
+        # 按下ESC时退出
+        if cv2.waitKey(1) == 27:
+            break
 
-        fig = plt.figure()
-        a = fig.add_subplot(2, 2, 1)
-        a.set_title('Result')
-        plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+    cv2.destroyAllWindows()
 
-        bgimg = cv2.cvtColor(image.astype(np.uint8), cv2.COLOR_BGR2RGB)
-        bgimg = cv2.resize(bgimg, (e.heatMat.shape[1], e.heatMat.shape[0]), interpolation=cv2.INTER_AREA)
-
-        # show network output
-        a = fig.add_subplot(2, 2, 2)
-        plt.imshow(bgimg, alpha=0.5)
-        tmp = np.amax(e.heatMat[:, :, :-1], axis=2)
-        plt.imshow(tmp, cmap=plt.cm.gray, alpha=0.5)
-        plt.colorbar()
-
-        tmp2 = e.pafMat.transpose((2, 0, 1))
-        tmp2_odd = np.amax(np.absolute(tmp2[::2, :, :]), axis=0)
-        tmp2_even = np.amax(np.absolute(tmp2[1::2, :, :]), axis=0)
-
-        a = fig.add_subplot(2, 2, 3)
-        a.set_title('Vectormap-x')
-        # plt.imshow(CocoPose.get_bgimg(inp, target_size=(vectmap.shape[1], vectmap.shape[0])), alpha=0.5)
-        plt.imshow(tmp2_odd, cmap=plt.cm.gray, alpha=0.5)
-        plt.colorbar()
-
-        a = fig.add_subplot(2, 2, 4)
-        a.set_title('Vectormap-y')
-        # plt.imshow(CocoPose.get_bgimg(inp, target_size=(vectmap.shape[1], vectmap.shape[0])), alpha=0.5)
-        plt.imshow(tmp2_even, cmap=plt.cm.gray, alpha=0.5)
-        plt.colorbar()
-        plt.show()
-    except Exception as e:
-        logger.warning('matplitlib error, %s' % e)
-        cv2.imshow('result', image)
-        cv2.waitKey()
+logger.debug('finished+')
